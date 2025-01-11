71601536f489ebd4150521c3912102584ad6d03a
Adil fareed <82055942+Adilfareed@users.noreply.github.com>
2025-01-11 20:08:08 +0500

    Completed Homepage

--- src/app/components/Navebar.jsx
+++ src/app/components/Navebar.jsx
@@ -7,28 +7,28 @@ const Navbar = () => {
     <nav className=" lg:grid grid-cols-3  px-6 py-4 hidden ">
 
       <div className='col-span-2'>
-        <h1 className='font-bold text-3xl'>Blog <span className='text-amber-900 '>Breeze</span></h1>
+        <h1 className='font-bold text-3xl p-3 '>Blog <span className='text-amber-900 '>Breeze</span></h1>
       </div>
       <div>
-      <ul className="flex space-x-6 p-4 justify-center items-center ">
-        <li>
+      <ul className="flex space-x-6 p-4 justify-center items-center text-2xl ">
+        <li className='font-semibold hover:text-amber-900'>
           <Link href="/" legacyBehavior>
-            <a className="hover:underline">Blogs</a>
+          Blogs
           </Link>
         </li>
-        <li>
+        <li className='font-semibold hover:text-amber-900'>
           <Link href="/about" legacyBehavior>
-            <a className="hover:underline">About Us</a>
+          About Us
           </Link>
         </li>
-        <li>
+        <li className='font-semibold hover:text-amber-900'>
           <Link href="/contact" legacyBehavior>
-            <a className="hover:underline">Contact</a>
+          Contact
           </Link>
         </li>
-        <li className='bg-amber-950 rounded-lg  text-white'>
+        <li className='bg-amber-950 rounded-lg p-3 text-white hover:bg-amber-900'>
           <Link href="/contact" legacyBehavior>
-            <button className="hover:bg-amber-800 p-2   ">Login</button>
+          Login
           </Link>
         </li>
       </ul>
--- src/app/page.js
+++ src/app/page.js
@@ -24,22 +24,25 @@ export default function Home() {
       <Navbar />
 
       <div className='flex flex-col justify-center items-center'>
-            <h1 className='text-3xl font-bold'> Our News </h1>
-            <p className='m-4 font-semibold'> Get our latest news first and increase your Knowledge about Current World </p>
+            <h1 className='text-[80px] font-bold'> Our News </h1>
+            <p className='m-4  text-2xl'> Get our latest news first and increase your Knowledge about Current World </p>
 
       </div>
       <div className='flex justify-center items-center'> 
-        <img  className='w-[80%]  h-[400px] object-cover' src='/blogging-main.jpg '/>
+        <img  className='w-[80%]  h-[450px] object-cover ' src='/blogging-main.jpg '/>
       </div>
       <div className="container mx-auto p-6">
-        <h1 className="text-2xl font-bold mb-4">Blogs</h1>
+    
         
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-1 gap-6">
           {blogs.map((blog) => (
-            <div key={blog.id} className="border rounded-lg p-4">
-              <img src={blog.image} alt={blog.title} className="w-full h-40 object-cover mb-4 rounded" />
-              <h2 className="text-xl font-bold mb-2">{blog.title}</h2>
-              <p>{blog.text}</p>
+            <div key={blog.id} className="border rounded-lg p-4 lg:flex">
+             <div> <img src={blog.image} alt={blog.title} className="w-72  h-40 object-cover mb-4 rounded" /></div>
+             <div >
+             <h2 className="text-4xl font-bold mb-2  p-4">{blog.title}</h2>
+             <p className='p-4' >{blog.text}</p>
+             </div>
+            
             </div>
           ))}
         </div>
